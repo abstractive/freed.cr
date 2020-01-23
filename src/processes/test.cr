36c404be659da4d143f21c1d47c23fc84d67672a
//de <code@extremist.digital>
2020-01-23 11:09:49 -0800

    clean up example vs. keep same as published one on shard

--- src/processes/test.cr
+++ src/processes/test.cr
@@ -7,6 +7,5 @@ this = Freed::Mind.new
 this["spies"].insert({ "name" => "James Bond", "age" => 37 })
 
 this["spies"].find({ "age" => { "$gt" => 30 } }) do |doc|
-  puts typeof(doc)    # => BSON
-  puts doc
+  puts "#{typeof(doc)}: #{doc}"
 end
