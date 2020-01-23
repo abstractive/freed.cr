47db3139ad2a3d18f8aa20fd1e13cf419bbc277e
//de <code@extremist.digital>
2020-01-23 11:02:20 -0800

    closes #8

--- src/freed/initialize.cr
+++ src/freed/initialize.cr
@@ -1,5 +1,6 @@
 require "totem"
-require "mongo" #de Expect MongoDB right now and make no effort to abstract it.
+require "mongo"         #de Expect MongoDB right now and make no effort to abstract it.
+require "msgpack"
 
 require "./exceptions"
 require "./mongo/*"
