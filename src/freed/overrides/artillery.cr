bfb980d2a4000435ebb7cd5e412b9b5c7b24077b
//de <code@extremist.digital>
2020-01-23 14:18:52 -0800

    remove log; change hame of hook Set

--- src/freed/overrides/artillery.cr
+++ src/freed/overrides/artillery.cr
@@ -7,19 +7,19 @@ module Artillery
     end
   end
   class Armory
-    @@hooks = Set(Symbol | Shell::MetaType).new
+    @@focuses = Set(Symbol | Shell::MetaType).new
     def self.organize
       organize { |vector|
         unless vector[:meta].nil?
-          if meta = vector[:meta]? #de [:data]
+          if meta = vector[:meta]?
             if (hooks = meta[:focuses]) && hooks.is_a?(Array)
               Artillery.log "Added #{vector[:method].to_s.upcase}#{vector[:path]} on #{vector[:object]} with data hooks: #{hooks} #{typeof(hooks)}"
-              hooks.each { |hook| @@hooks.add(hook)}
+              hooks.each { |hook| @@focuses.add(hook)}
             end
           end
         end
       }
-      Artillery.log( "Hooks present: #{@@hooks}")
+      Artillery.log( "Focus hooks present: #{@@focuses}")
     end
   end
 end
